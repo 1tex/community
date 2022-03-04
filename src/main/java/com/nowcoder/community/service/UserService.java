a0ad20588a7b75df9baa8569817f4a69fe832ad3
tex1 <ty.zhao@live.com>
2022-03-04 11:25:27 +0800

    上传头像

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -166,4 +166,8 @@ public class UserService implements CommunityConstant {
         return loginTicketMapper.selectByTicket(ticket);
     }
 
+    public int updateHeader(int userId, String headerUrl) {
+        return userMapper.updateHeader(userId, headerUrl);
+    }
+
 }
